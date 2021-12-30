1fed61592b1dfac079626410a204912e67614d6d
Charles <tietjen.charles@gmail.com>
2021-12-30 14:37:22 -0500

    Delete charlesScript.js

--- assets/js/charlesScript.js
+++
@@ -1,9 +0,0 @@
-//edamam key e33914458ccf91189cbbbdaf6ce48cb1
-//edam appID = 
-//cuisineType string = American
-
-//thecocktaildb api key is "1" - educational key
-//random cocktail - www.thecocktaildb.com/api/json/v1/1/random.php
-//By ingredient - www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin
-//By category - www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail
-
