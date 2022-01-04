2b5ba8c3b53cd24633b4e0e82002f1c61dfca9a3
sajees89 <92955815+sajees89@users.noreply.github.com>
2022-01-04 17:41:02 -0500

    Update localScript.js

--- assets/js/localScript.js
+++ assets/js/localScript.js
@@ -6,6 +6,7 @@ console.log('Restaurant and Recipe Finder');
 
 
 
+
 const recipeModalButtons = document.querySelectorAll("[data-modal-target]")
 const closeModalButtons = document.querySelectorAll("[data-close-button]")
 const overlay = document.getElementById("overlay") 
@@ -16,33 +17,33 @@ recipeModalButtons.forEach(button => {
     const modal = document.querySelector(button.dataset.modalTarget)
     recipeModal(modal)
   })
-})
+}); 
 
 overlay.addEventListener("click", () => {
   const modals = document.querySelectorAll(".modal.active")
   modals.forEach(modal => {
     closeModal(modal)
   })
-})
+}); 
 
 closeModalButtons.forEach(button => {
   button.addEventListener('click', () => {
     const modal = button.closest(".modal")
     closeModal(modal)
   })
-})
+}); 
 
 function recipeModal(modal) { 
   if (modal === null) return 
   modal.classList.add("active")
   overlay.classList.add(active)
-}
+}; 
 
 function closeModal(modal) { 
   if (modal === null) return 
   modal.classList.remove("active")
   overlay.classList.remove("active")
-}
+}; 
 
 
 
