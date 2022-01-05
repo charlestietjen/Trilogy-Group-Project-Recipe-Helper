be562e86bf1b70ba7f404cdc1aeb834f54ce148d
Charles T <tietjen.charles@gmail.com>
2022-01-05 16:43:45 -0500

    quick update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
-let searchButton = document.querySelector("#search")
-let searchDrink = document.querySelector("#drink")
+let searchButton = document.querySelector("#recipes")
+let searchDrink = document.querySelector("#cocktail")
 
 searchButton.addEventListener("click", () => {
   console.log('recipe pressed')
@@ -64,3 +64,61 @@ function useApiData2(data2){
 </div>
   `
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Modal script
+// Default dropdown value
+const DEFAULT_DROPDOWN_VALUE = 'Cuisine';
+
+// Display selected item from dropdown
+$('.dropdown-menu li a').click(function () {
+    $('#cuisine').html($(this).text());
+});
+
+// Hide search section and show result
+const smashBtn = document.querySelector('#smash');
+const cuisineContainer = document.querySelector('#cuisineContainer');
+smashBtn.addEventListener('click', function (event) {
+    cuisineContainer.classList.add('hide');
+    resultSection.classList.remove('hide');
+    document.querySelector('#cuisine').textContent = DEFAULT_DROPDOWN_VALUE;
+});
+
+// Show search section and hide result
+const resultSection = document.querySelector('#result');
+const goHomeBtn = document.querySelector('#gohome');
+goHomeBtn.addEventListener('click', function (event) {
+    cuisineContainer.classList.remove('hide');
+    resultSection.classList.add('hide');
+});
