1acc5a68bcb16fe10afa922d7c54fa706e9140c4
Abirami Theepan <abira0607@gmail.com>
2022-01-05 11:46:09 -0500

    sample UI model with Bootstrap5

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,24 @@
-// JS 1
-console.log('Restaurant and Recipe Finder');
+// Default dropdown value
+const DEFAULT_DROPDOWN_VALUE = 'Cuisine';
 
-// JS 2
+// Display selected item from dropdown
+$('.dropdown-menu li a').click(function () {
+    $('#cuisine').html($(this).text());
+});
+
+// Hide search section and show result
+const smashBtn = document.querySelector('#smash');
+const cuisineContainer = document.querySelector('#cuisineContainer');
+smashBtn.addEventListener('click', function (event) {
+    cuisineContainer.classList.add('hide');
+    resultSection.classList.remove('hide');
+    document.querySelector('#cuisine').textContent = DEFAULT_DROPDOWN_VALUE;
+});
+
+// Show search section and hide result
+const resultSection = document.querySelector('#result');
+const goHomeBtn = document.querySelector('#gohome');
+goHomeBtn.addEventListener('click', function (event) {
+    cuisineContainer.classList.remove('hide');
+    resultSection.classList.add('hide');
+});
